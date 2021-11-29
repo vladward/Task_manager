214d4781ed2ba2e2b02a61418afa3dbe164f64a2
vladward <vladward13@gmail.com>
2021-11-29 20:27:37 +0300

    fix: comment old tests

--- src/tests/reducer.test.ts
+++ src/tests/reducer.test.ts
@@ -1,12 +1,12 @@
 import {ActionType, calculator} from "./reducer";
 
-test("test reducer", () => {
-    // 1. Получаем тестовые данные
-  const num1 = 5
-  const num2 = 6
-    // 2. Выполнение тестируемого кода
-  let action: ActionType = {type: "sum", number: 6}
-
-  // 3. Сравнение с ожидаемым результатом
-  expect(calculator(5, action)).toBe(11)
-})
+// test("test reducer", () => {
+//     // 1. Получаем тестовые данные
+//   const num1 = 5
+//   const num2 = 6
+//     // 2. Выполнение тестируемого кода
+//   let action: ActionType = {type: "sum", number: 6}
+//
+//   // 3. Сравнение с ожидаемым результатом
+//   expect(calculator(5, action)).toBe(11)
+// })
