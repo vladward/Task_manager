acdd9af80c926679cdedb6a6f52a901e2faa19f0
vladward <vladward13@gmail.com>
2021-12-05 17:15:50 +0300

    feat: add test change task status for tasksReducer

--- src/tests/tasks_reducer_tests/tasksReducer.test.ts
+++ src/tests/tasks_reducer_tests/tasksReducer.test.ts
@@ -1,7 +1,7 @@
 import {useState} from "react";
 import {v1} from "uuid";
 import {TasksStateType, TodoListType} from "../../App";
-import {addTaskAC, removeTaskAC, tasksReducer} from "./tasksReducer";
+import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, tasksReducer} from "./tasksReducer";
 
 test('remove task from todolist', () => {
     const todoListId_1 = v1()
@@ -62,4 +62,35 @@ test('add task to todolist', () => {
     expect(endState[todoListId_1][0].title).toBe("WEB")
     expect(endState[todoListId_2].length).toBe(4)
     expect(endState[todoListId_1].length).toBe(5)
+})
+
+test('change Task Status', () => {
+    const todoListId_1 = v1()
+    const todoListId_2 = v1()
+
+
+    const startState: TasksStateType = {
+        [todoListId_1]: [
+            {id: v1(), title: "HTML", isDone: true},
+            {id: v1(), title: "CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: false},
+            {id: v1(), title: "REACT", isDone: false}
+        ],
+        [todoListId_2]: [
+            {id: v1(), title: "Meat", isDone: true},
+            {id: v1(), title: "Milk", isDone: true},
+            {id: v1(), title: "Cheese", isDone: false},
+            {id: v1(), title: "Beer", isDone: false},
+        ]
+    }
+
+    const ID = startState[todoListId_1][0].id
+
+    const endState = tasksReducer(startState, changeTaskStatusAC(ID, todoListId_1, false))
+
+    expect(endState[todoListId_1][0].isDone).toBe(false)
+    expect(endState[todoListId_2][0].isDone).toBe(true)
+    expect(endState[todoListId_2].length).toBe(4)
+    expect(endState[todoListId_1].length).toBe(4)
+    console.log(endState)
 })
