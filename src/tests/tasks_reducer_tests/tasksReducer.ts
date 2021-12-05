41e7f6360644e5c223c0fe4c98dfcb0f59cfd150
vladward <vladward13@gmail.com>
2021-12-05 16:58:52 +0300

    fix: add tasksReducer addTask case code, type, AC

--- src/tests/tasks_reducer_tests/tasksReducer.ts
+++ src/tests/tasks_reducer_tests/tasksReducer.ts
@@ -1,18 +1,30 @@
 import {TasksStateType} from "../../App";
+import {v1} from "uuid";
 
 type removeTasksType = {
     type: "REMOVE-TASKS"
     taskId: string
     todoListID: string
 }
+type addTasksType = {
+    type: "ADD-TASKS"
+    taskId: string
+    todoListID: string
+    title: string
+}
 
-type TasksActionType = removeTasksType
+type TasksActionType = removeTasksType | addTasksType
 
 export const tasksReducer = (state: TasksStateType, action: TasksActionType): TasksStateType => {
     switch (action.type) {
         case "REMOVE-TASKS": {
             let copy = {...state}
             return {...copy, [action.todoListID]: copy[action.todoListID].filter(t => t.id !== action.taskId)}
+        }
+        case "ADD-TASKS": {
+            let copy = {...state}
+            copy[action.todoListID] = [{id: v1(), title: action.title, isDone: false}, ...copy[action.todoListID]]
+            return copy
         }
         default:
             return state
@@ -21,4 +33,7 @@ export const tasksReducer = (state: TasksStateType, action: TasksActionType): Ta
 
 export const removeTaskAC = (taskId: string, todoListID: string): removeTasksType => {
     return {type: "REMOVE-TASKS", taskId, todoListID} as const
+}
+export const addTaskAC = (taskId: string, todoListID: string, title: string): addTasksType => {
+    return {type: "ADD-TASKS", taskId, todoListID, title} as const
 }
