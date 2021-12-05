90be4a1c3fd48ea5bf908e3030b44cc1f7427e1e
vladward <vladward13@gmail.com>
2021-12-05 17:16:14 +0300

    feat: add test change task title for tasksReducer

--- src/tests/tasks_reducer_tests/tasksReducer.test.ts
+++ src/tests/tasks_reducer_tests/tasksReducer.test.ts
@@ -94,3 +94,35 @@ test('change Task Status', () => {
     expect(endState[todoListId_1].length).toBe(4)
     console.log(endState)
 })
+
+test('change Task title', () => {
+    const todoListId_1 = v1()
+    const todoListId_2 = v1()
+
+
+    const startState: TasksStateType = {
+        [todoListId_1]: [
+            {id: v1(), title: "HTML", isDone: true},
+            {id: v1(), title: "CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: false},
+            {id: v1(), title: "REACT", isDone: false}
+        ],
+        [todoListId_2]: [
+            {id: v1(), title: "Meat", isDone: true},
+            {id: v1(), title: "Milk", isDone: true},
+            {id: v1(), title: "Cheese", isDone: false},
+            {id: v1(), title: "Beer", isDone: false},
+        ]
+    }
+
+    const newTitle = 'Vodka'
+    const ID = startState[todoListId_2][1].id
+
+    const endState = tasksReducer(startState, changeTaskTitleAC(ID, todoListId_2, newTitle))
+
+    expect(endState[todoListId_1][1].title).toBe("CSS")
+    expect(endState[todoListId_2][1].title).toBe("Vodka")
+    expect(endState[todoListId_2].length).toBe(4)
+    expect(endState[todoListId_1].length).toBe(4)
+    console.log(endState)
+})
