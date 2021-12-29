ee705e1272af672aaae25c88f8e1c20f92a161e2
vladward <vladward13@gmail.com>
2021-12-30 01:12:52 +0300

    feat: add App stories

---
+++ src/App.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import App from "./App";
+import {ReduxStoreProviderDecorator} from "./stories/decorators/ReduxStoreProviderDecorator";
+
+
+export default {
+  title: 'Todo/App',
+  component: App,
+    decorators: [ReduxStoreProviderDecorator],
+    args: {}
+} as ComponentMeta<typeof App>;
+
+const Template: ComponentStory<typeof App> = (args) => <App />
+
+export const AppExample = Template.bind({})
+
