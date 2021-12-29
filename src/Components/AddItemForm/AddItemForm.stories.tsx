b295ffaa7339951d352489cd522a7b7ffef64be9
vladward <vladward13@gmail.com>
2021-12-30 01:12:41 +0300

    feat: add AddItemForm stories

---
+++ src/Components/AddItemForm/AddItemForm.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import {AddItemForm} from "./AddItemForm";
+import {action} from "@storybook/addon-actions";
+
+
+export default {
+  title: 'Todo/AddItemForm',
+  component: AddItemForm,
+  argTypes: {
+    onClick: {
+      description: 'Button inside form clicked'
+    }
+  },
+} as ComponentMeta<typeof AddItemForm>;
+
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
+
+export const AddItemFormExample = Template.bind({});
+AddItemFormExample.args = {
+  addItem: action('Button inside form clicked')
+}
