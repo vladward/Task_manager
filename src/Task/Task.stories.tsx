b95b1344d2f142b4c27078dabb99f376553d11a9
vladward <vladward13@gmail.com>
2021-12-30 01:13:11 +0300

    feat: add Task stories

---
+++ src/Task/Task.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {Task} from "./Task";
+import {ReduxStoreProviderDecorator} from "../stories/decorators/ReduxStoreProviderDecorator";
+
+
+export default {
+  title: 'Todo/Task',
+  component: Task,
+    decorators: [ReduxStoreProviderDecorator],
+    args: {
+        todolistId: 'todolistId1',
+        taskId: '1'
+    }
+} as ComponentMeta<typeof Task>;
+
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />
+
+export const TaskExample = Template.bind({})
+
