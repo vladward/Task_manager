b1e14edcf60cf66371df493cc125f63ae6e93615
vladward <vladward13@gmail.com>
2022-01-06 22:11:11 +0300

    ref: refactor requests with Todo api methods

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,20 +1,17 @@
 import React, {useEffect, useState} from 'react'
+import {TodoApi} from "../api/todo-api";
 
 export default {
     title: 'API'
 }
 
-const settings = {
-    withCredentials: true
-}
-
-
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-
-
-
+        TodoApi.getTodolist()
+            .then((res) => {
+                setState(res.data);
+            })
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
@@ -22,6 +19,11 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        let title = "newTodolist1111111"
+        TodoApi.createTodolist(title)
+            .then((res) => {
+                setState(res.data);
+            })
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
@@ -29,13 +31,25 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        let todoId = 'f04e0bf1-c4e2-4c63-8629-6ae8c3068840'
+        TodoApi.deleteTodolist(todoId)
+            .then((res) => {
+                setState(res.data);
+            })
+
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
 }
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
+    let title = 'React-redux-ty-znat-budesh-crutaaaas'
+    let todoId = 'f04e0bf1-c4e2-4c63-8629-6ae8c3068840'
     useEffect(() => {
+        TodoApi.updateTodolist({todoId, title})
+            .then((res) => {
+                setState(res.data);
+            })
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
