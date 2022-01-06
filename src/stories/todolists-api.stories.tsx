8a1435ce7310309ac41aa91d6a7e40429e9d8843
vladward <vladward13@gmail.com>
2022-01-06 19:15:14 +0300

    feat: create todolist api stories

---
+++ src/stories/todolists-api.stories.tsx
@@ -0,0 +1,43 @@
+import React, {useEffect, useState} from 'react'
+
+export default {
+    title: 'API'
+}
+
+const settings = {
+    withCredentials: true
+}
+
+
+export const GetTodolists = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+
+
+
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const DeleteTodolist = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+export const UpdateTodolistTitle = () => {
+    const [state, setState] = useState<any>(null)
+    useEffect(() => {
+    }, [])
+
+    return <div> {JSON.stringify(state)}</div>
+}
+
