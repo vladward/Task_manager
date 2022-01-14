3a89cf131ae68f2e29afbd38337452c868597fa8
vladward <vladward13@gmail.com>
2022-01-15 02:31:14 +0300

    upd: update todolists reducer with thunk creators

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,23 +1,21 @@
-import { v1 } from 'uuid';
 import {todolistsAPI, TodolistType} from '../api/todolists-api'
 import {Dispatch} from "redux";
 
 export type RemoveTodolistActionType = {
-    type: 'REMOVE-TODOLIST',
-    id: string
-}
-export type AddTodolistActionType = {
-    type: 'ADD-TODOLIST',
-    title: string
+    type: 'REMOVE-TODOLIST'
     todolistId: string
 }
+export type AddTodolistActionType = {
+    type: 'ADD-TODOLIST'
+    todolist: TodolistType
+}
 export type ChangeTodolistTitleActionType = {
-    type: 'CHANGE-TODOLIST-TITLE',
+    type: 'CHANGE-TODOLIST-TITLE'
     id: string
     title: string
 }
 export type ChangeTodolistFilterActionType = {
-    type: 'CHANGE-TODOLIST-FILTER',
+    type: 'CHANGE-TODOLIST-FILTER'
     id: string
     filter: FilterValuesType
 }
@@ -45,16 +43,11 @@ export type TodolistDomainType = TodolistType & {
 export const todolistsReducer = (state: Array<TodolistDomainType> = initialState, action: ActionsType): Array<TodolistDomainType> => {
     switch (action.type) {
         case 'REMOVE-TODOLIST': {
-            return state.filter(tl => tl.id !== action.id)
+            return state.filter(tl => tl.id !== action.todolistId)
         }
         case 'ADD-TODOLIST': {
-            return [{
-                id: action.todolistId,
-                title: action.title,
-                filter: 'all',
-                addedDate: '',
-                order: 0
-            }, ...state]
+            let todo: TodolistDomainType = {...action.todolist, filter: 'all'}
+            return [todo, ...state]
         }
         case 'CHANGE-TODOLIST-TITLE': {
             const todolist = state.find(tl => tl.id === action.id);
@@ -82,10 +75,10 @@ export const todolistsReducer = (state: Array<TodolistDomainType> = initialState
 }
 
 export const removeTodolistAC = (todolistId: string): RemoveTodolistActionType => {
-    return {type: 'REMOVE-TODOLIST', id: todolistId}
+    return {type: 'REMOVE-TODOLIST', todolistId}
 }
-export const addTodolistAC = (title: string): AddTodolistActionType => {
-    return {type: 'ADD-TODOLIST', title: title, todolistId: v1()}
+export const addTodolistAC = (todolist: TodolistType): AddTodolistActionType => {
+    return {type: 'ADD-TODOLIST', todolist}
 }
 export const changeTodolistTitleAC = (id: string, title: string): ChangeTodolistTitleActionType => {
     return {type: 'CHANGE-TODOLIST-TITLE', id: id, title: title}
@@ -104,18 +97,46 @@ export const setTodolists = () => {
                 dispatch(setTodolistsAC(res.data))
             })
             .catch(err => {
-                console.log('Error ', err)
+                console.log('Error adding a to-do ', err)
             })
     }
 }
-export const removeTodolist = () => {
+export const removeTodolistTC = (id: string) => {
     return (dispatch: Dispatch) => {
-        todolistsAPI.getTodolists()
+        todolistsAPI.deleteTodolist(id)
             .then(res => {
-                dispatch(setTodolistsAC(res.data))
+                if (res.data.resultCode === 0) {
+                    dispatch(removeTodolistAC(id))
+                }
             })
             .catch(err => {
-                console.log('Error ', err)
+                console.log('To-do has not been removed ', err)
+            })
+    }
+}
+export const updateTodolistTitle = (id: string, title: string) => {
+    return (dispatch: Dispatch) => {
+        todolistsAPI.updateTodolist(id, title)
+            .then(res => {
+                if (res.data.resultCode === 0) {
+                    dispatch(changeTodolistTitleAC(id, title))
+                }
+            })
+            .catch(err => {
+                console.log('To-do title not updated ', err)
+            })
+    }
+}
+export const createTodolist = (title: string) => {
+    return (dispatch: Dispatch) => {
+        todolistsAPI.createTodolist(title)
+            .then(res => {
+                if (res.data.resultCode === 0) {
+                    dispatch(addTodolistAC(res.data.data.item))
+                }
+            })
+            .catch(err => {
+                console.log("To-do wasn't added ", err)
             })
     }
 }
