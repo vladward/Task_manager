aa356634834a8653b3bf6c62ddfcc443b5c0b47b
vladward <vladward13@gmail.com>
2022-01-14 23:15:47 +0300

    ref: AddItemForm

--- src/Components/AddItemForm/AddItemForm.tsx
+++ src/Components/AddItemForm/AddItemForm.tsx
@@ -1,51 +1,48 @@
-import React, {ChangeEvent, KeyboardEvent, useState} from "react"
+import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 import {IconButton, TextField} from "@material-ui/core";
 import {AddBox} from "@material-ui/icons";
 
-type AddItemFormType = {
+type AddItemFormPropsType = {
     addItem: (title: string) => void
-    initValue?: string
 }
 
-export const AddItemForm = React.memo((props: AddItemFormType) => {
-    const [title, setTitle] = useState<string>("")
-    const [error, setError] = useState<boolean>(false)
+export const AddItemForm = React.memo(function (props: AddItemFormPropsType) {
+    const [title, setTitle] = useState('')
+    const [error, setError] = useState<string | null>(null)
 
-    console.log('AddItemForm')
-
-    const AddItem = () => {
-        title.trim() !== ''
-            ? props.addItem(title.trim())
-            : setError(true)
-        setTitle("")
+    const addItem = () => {
+        if (title.trim() !== '') {
+            props.addItem(title)
+            setTitle('')
+        } else {
+            setError('Title is required')
+        }
     }
 
-    const onChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.currentTarget.value) {
-            setError(false)
-        }
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         setTitle(e.currentTarget.value)
     }
 
-    const onKeyPressEnter = (e: KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === "Enter") {
-            AddItem()
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (error !== null) {
+            setError(null)
+        }
+        if (e.charCode === 13) {
+            addItem()
         }
     }
 
-    return (
-        <div>
-            <TextField label={!error ? (props.initValue ? props.initValue : "Enter title") : "Error"}
-                       size="small"
-                       variant="outlined"
-                       error={error}
-                       helperText={error ? "Title is required" : ""}
-                       onChange={onChangeTitle}
-                       value={title}
-                       onKeyPress={onKeyPressEnter}/>
-            <IconButton aria-label="delete" onClick={AddItem} style={{color: "green"}} disabled={!title}>
-                <AddBox fontSize="small" />
-            </IconButton>
-        </div>
-    )
+    return <div>
+        <TextField variant="outlined"
+                   error={!!error}
+                   value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}
+                   label="Title"
+                   helperText={error}
+        />
+        <IconButton color="primary" onClick={addItem}>
+            <AddBox/>
+        </IconButton>
+    </div>
 })
