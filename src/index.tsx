a3ea6371deabb1117cdafcf0bc7d19b6b07e72a5
vladward <vladward13@gmail.com>
2022-01-14 23:21:56 +0300

    ref: index

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {Provider} from "react-redux";
-import {store} from "./state/store";
+import {Provider} from 'react-redux';
+import {store} from './state/store';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
-    document.getElementById('root'));
-
-serviceWorker.unregister();
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
