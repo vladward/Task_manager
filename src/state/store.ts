1f10d6893476fc0c683b23d27040710205d85b7d
vladward <vladward13@gmail.com>
2022-01-28 18:14:34 +0300

    feat: create auth reducer

--- src/state/store.ts
+++ src/state/store.ts
@@ -4,11 +4,13 @@ import {applyMiddleware, combineReducers, createStore} from 'redux';
 import thunk from 'redux-thunk'
 import {appReducer} from "../App/appReducer";
 import {TypedUseSelectorHook, useSelector} from "react-redux";
+import {authReducer} from "../features/Login/authReducer";
 
 const rootReducer = combineReducers({
     todolists: todolistsReducer,
     tasks: tasksReducer,
     app: appReducer,
+    auth: authReducer
 })
 export const store = createStore(rootReducer, applyMiddleware(thunk))
 export type AppRootStateType = ReturnType<typeof rootReducer>
