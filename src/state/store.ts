0d11e7145bf6d696d09584c9b81dbca16857a6c5
vladward <vladward13@gmail.com>
2022-01-29 17:31:16 +0300

    feat: add global thunk type

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,10 +1,10 @@
-import { tasksReducer } from './tasks-reducer';
-import { todolistsReducer } from './todolists-reducer';
+import {TasksActionsType, tasksReducer} from './tasks-reducer';
+import {TodolistsActionsType, todolistsReducer} from './todolists-reducer';
 import {applyMiddleware, combineReducers, createStore} from 'redux';
-import thunk from 'redux-thunk'
-import {appReducer} from "../App/appReducer";
+import thunk, { ThunkAction } from 'redux-thunk'
+import {AppActionsType, appReducer} from "../App/appReducer";
 import {TypedUseSelectorHook, useSelector} from "react-redux";
-import {authReducer} from "../features/Login/authReducer";
+import {AuthActionsType, authReducer} from "../features/Login/authReducer";
 
 const rootReducer = combineReducers({
     todolists: todolistsReducer,
@@ -15,6 +15,8 @@ const rootReducer = combineReducers({
 export const store = createStore(rootReducer, applyMiddleware(thunk))
 export type AppRootStateType = ReturnType<typeof rootReducer>
 export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
+export type ReducersActionTypes = AppActionsType | AuthActionsType | TodolistsActionsType | TasksActionsType
+export type ThunkType = ThunkAction<void, AppRootStateType, unknown, ReducersActionTypes>
 
 // @ts-ignore
 window.store = store;
