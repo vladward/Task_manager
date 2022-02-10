8eeb09030bdf22cc2477459c0a40245bb36a69ae
vladward <vladward13@gmail.com>
2022-02-10 16:22:31 +0300

    ref: changeFilterAC params

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -21,7 +21,6 @@ type PropsType = {
     demo?: boolean
 }
 
-
 export const TodolistsList: React.FC<PropsType> = ({demo = false}) => {
 
     const dispatch = useDispatch()
@@ -59,7 +58,7 @@ export const TodolistsList: React.FC<PropsType> = ({demo = false}) => {
     }, [])
 
     const changeFilter = useCallback(function (value: FilterValuesType, todolistId: string) {
-        const action = changeTodolistFilterAC(todolistId, value)
+        const action = changeTodolistFilterAC({id: todolistId, filter: value})
         dispatch(action)
     }, [])
 
